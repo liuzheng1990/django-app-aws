7e405d17f934832bd12f799a99ff23e2ad7b32e3
Liu Zheng <liuzheng900204@gmail.com>
2023-06-11 00:26:22 +0800

    CD: push image to ECR

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    def __repr__(self):
+        return f"Question: {self.question_text}"
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __repr__(self):
+        return f"Choice: {self.choice_text}"
