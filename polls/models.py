8444ec6d29231b0e587687d1fd6d45296db11fcb
Liu Zheng <liuzheng900204@gmail.com>
2023-06-12 01:41:12 +0800

    fixed linting errors

--- polls/models.py
+++ polls/models.py
@@ -16,3 +16,4 @@ class Choice(models.Model):
 
     def __repr__(self):
         return f"Choice: {self.choice_text}"
+    
--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import QuestionListView, QuestionDetailView
+from .views import QuestionDetailView, QuestionListView
 
 
 urlpatterns = [
