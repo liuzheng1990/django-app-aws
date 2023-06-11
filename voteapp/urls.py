f3bfec9941f1f3ab0dca65b1c2c0f720468c6536
Liu Zheng <liuzheng900204@gmail.com>
2023-06-12 02:13:04 +0800

    black linter is the worst

--- voteapp/urls.py
+++ voteapp/urls.py
@@ -21,5 +21,5 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),
-    path("", RedirectView.as_view(url="polls/"))
+    path("", RedirectView.as_view(url="polls/")),
 ]
