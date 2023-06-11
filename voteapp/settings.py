2c253234bf6c04a0e9424e96c8e84b80d996095c
Liu Zheng <liuzheng900204@gmail.com>
2023-06-11 21:41:17 +0800

    fixed ALLOWED_HOSTS and service name

--- voteapp/settings.py
+++ voteapp/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", config['Django']['secret_key'])
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", config['Django'].getboolean('debug'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
